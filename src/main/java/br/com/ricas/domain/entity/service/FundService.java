27dbbcb45beb1424a347e082153293e7bf6a4996
Ricardo <ricardohsmello@gmail.com>
2023-10-26 20:38:37 -0300

    create filter greater than

--- src/main/java/br/com/ricas/domain/entity/service/FundService.java
+++ src/main/java/br/com/ricas/domain/entity/service/FundService.java
@@ -1,7 +1,11 @@
 package br.com.ricas.domain.entity.service;
 
 import br.com.ricas.domain.entity.Fund;
+import org.bson.Document;
+
+import java.util.List;
 
 public interface FundService {
      void create(Fund fund);
+     List<Fund> filterGreaterThan100();
 }
