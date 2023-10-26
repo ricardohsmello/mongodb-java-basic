1110f65faaed9966ed9ab57bb84e62e4fd523eb4
Ricardo <ricardohsmello@gmail.com>
2023-10-26 20:10:12 -0300

    create project and create methods

---
+++ src/main/java/br/com/ricas/application/config/MongoConfig.java
@@ -0,0 +1,35 @@
+package br.com.ricas.application.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import java.util.logging.Logger;
+
+public class MongoConfig {
+
+    private static final String CONNECTION_STRING = "";
+    private static MongoClient mongoClient;
+    private static final Logger logger = Logger.getLogger(String.valueOf(MongoConfig.class));
+
+
+    // Private constructor to prevent direct instantiation
+    private MongoConfig() {
+        try {
+            mongoClient = MongoClients.create(CONNECTION_STRING);
+        }catch (Exception exception) {
+            logger.info(exception.getLocalizedMessage());
+        }
+    }
+
+    // Public method to get the instance of the MongoClient
+    public static MongoClient getInstance() {
+        if (mongoClient == null) {
+            synchronized (MongoConfig.class) {
+                if (mongoClient == null) {
+                    new MongoConfig();
+                }
+            }
+        }
+        return mongoClient;
+    }
+}
