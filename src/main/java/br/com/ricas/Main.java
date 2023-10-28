d77f9ab10236b836341158545667e327f9f67b8a
Ricardo <ricardohsmello@gmail.com>
2023-10-28 18:35:04 -0300

    create aggregate

--- src/main/java/br/com/ricas/Main.java
+++ src/main/java/br/com/ricas/Main.java
@@ -30,7 +30,7 @@ public class Main {
 //        deleteMany();
 //        filterAggregate();
 //        filterAggregateGroupExportedLanguage();
-//       filterAggregateGroup();
+//        filterAggregateGroup();
         filterSortWithProject();
     }
 
