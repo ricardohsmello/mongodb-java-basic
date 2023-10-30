517249002ad3c4bdca5901041477991d32f355ca
Ricardo <ricardohsmello@gmail.com>
2023-10-29 21:22:20 -0300

    create  aggregate

--- src/main/java/br/com/ricas/domain/entity/service/FundServiceImpl.java
+++ src/main/java/br/com/ricas/domain/entity/service/FundServiceImpl.java
@@ -21,7 +21,8 @@ import static com.mongodb.client.model.Filters.gte;
 
 public class FundServiceImpl implements FundService {
     private static final Logger logger = Logger.getLogger(String.valueOf(MongoConfig.class));
-    public MongoCollection<Document> fundsCollection = MongoConfig.getInstance().getDatabase(DatabaseEnum.CERTIFICATION.name().toLowerCase()).getCollection(CollectionEnum.FUNDS.name().toLowerCase());
+    public MongoCollection<Document> fundsCollection =
+            MongoConfig.getInstance().getDatabase(DatabaseEnum.CERTIFICATION.name().toLowerCase()).getCollection(CollectionEnum.FUNDS.name().toLowerCase());
 
     @Override
     public void insertOne(Fund fund) {
