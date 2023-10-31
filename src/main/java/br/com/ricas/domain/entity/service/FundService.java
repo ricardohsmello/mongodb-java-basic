08e5b97059241a668c6a3d7d468481f8ca4e2741
Ricardo <ricardohsmello@gmail.com>
2023-10-31 02:07:26 -0300

    create  aggregate

--- src/main/java/br/com/ricas/domain/entity/service/FundService.java
+++ src/main/java/br/com/ricas/domain/entity/service/FundService.java
@@ -3,11 +3,9 @@ package br.com.ricas.domain.entity.service;
 import br.com.ricas.domain.entity.Fund;
 import java.util.Date;
 import java.util.List;
-
 public interface FundService {
      void insertOne(Fund fund);
      List<Fund> filterGreaterThan100();
-
      Fund findFirst(String name);
 
      void updateOne(Fund fund);
