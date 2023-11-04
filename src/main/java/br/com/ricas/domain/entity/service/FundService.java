eb9b4804d72e0d03a7c511a4f6ba9ed9574fee2c
Ricardo <ricardohsmello@gmail.com>
2023-11-04 15:14:04 -0300

    create index

--- src/main/java/br/com/ricas/domain/entity/service/FundService.java
+++ src/main/java/br/com/ricas/domain/entity/service/FundService.java
@@ -3,7 +3,6 @@ package br.com.ricas.domain.entity.service;
 import br.com.ricas.domain.entity.Fund;
 import java.util.Date;
 import java.util.List;
-
 public interface FundService {
      void insertOne(Fund fund);
      List<Fund> filterGreaterThan100();
