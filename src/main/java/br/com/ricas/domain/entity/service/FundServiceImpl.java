cc9f3ef5412d54e27f50be9434427548e14dd308
Ricardo <ricardohsmello@gmail.com>
2023-11-06 23:27:42 -0300

    create index

--- src/main/java/br/com/ricas/domain/entity/service/FundServiceImpl.java
+++ src/main/java/br/com/ricas/domain/entity/service/FundServiceImpl.java
@@ -175,7 +175,6 @@ public class FundServiceImpl implements FundService {
         Bson eq = Filters.eq(FundsFieldEnum.NAME.name().toLowerCase(), "03");
         Bson sort = Aggregates.sort(Sorts.descending(FundsFieldEnum.VALUE.name().toLowerCase()));
         Bson project = Aggregates.project(Projections.exclude("_id"));
-
         AggregateIterable<Document> aggregate = fundsCollection.aggregate(List.of(
                         Aggregates.match(eq),
                         sort,
