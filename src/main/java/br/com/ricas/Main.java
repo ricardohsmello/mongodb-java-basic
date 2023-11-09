d24dfb4fc9abac17f1d7ae2d5ae6aec79450f3f8
Ricardo <ricardohsmello@gmail.com>
2023-11-09 18:52:01 -0300

    create index

--- src/main/java/br/com/ricas/Main.java
+++ src/main/java/br/com/ricas/Main.java
@@ -31,7 +31,6 @@ public class Main {
 //        filterAggregate();
 //        filterAggregateGroupExportedLanguage();
 //        filterAggregateGroup();
-//        filterSortWithProject();
 
         createCollection();
     }
