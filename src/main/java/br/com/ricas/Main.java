906348180040514a60448e1f569c202eae28394b
Ricardo <ricardohsmello@gmail.com>
2023-11-09 18:51:57 -0300

    create index

--- src/main/java/br/com/ricas/Main.java
+++ src/main/java/br/com/ricas/Main.java
@@ -32,7 +32,7 @@ public class Main {
 //        filterAggregateGroupExportedLanguage();
 //        filterAggregateGroup();
 //        filterSortWithProject();
-//        createIndex();
+
         createCollection();
     }
 
