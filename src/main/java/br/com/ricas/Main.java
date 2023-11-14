e6896bb7f951acfcbed8bf8390c57209e0c9e0da
Ricardo <ricardohsmello@gmail.com>
2023-11-14 19:05:03 -0300

    create index

--- src/main/java/br/com/ricas/Main.java
+++ src/main/java/br/com/ricas/Main.java
@@ -3,7 +3,6 @@ package br.com.ricas;
 import br.com.ricas.application.config.MongoConfig;
 import br.com.ricas.domain.entity.Fund;
 import br.com.ricas.domain.entity.service.FundServiceImpl;
-
 import java.util.Date;
 
 public class Main {
