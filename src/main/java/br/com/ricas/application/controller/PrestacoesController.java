e5e5546ca7bf73858560100aae16a3b028c63241
Ricardo <ricardohsmello@gmail.com>
2023-12-06 12:32:51 -0300

    create index

--- src/main/java/br/com/ricas/application/controller/PrestacoesController.java
+++ src/main/java/br/com/ricas/application/controller/PrestacoesController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrestacoesController {
-
     @GetMapping("/greeting")
     public String test() {
         return "ola";
